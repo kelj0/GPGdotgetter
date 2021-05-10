b00bbce3150cedc405e891a432042466defb5168
kelj0 <kelj0@protonmail.com>
2021-05-11 00:24:16 +0200

    add models

--- savedots/app/models.py
+++ savedots/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    email = models.EmailField()
+    password = models.CharField()
+    
+class Dotfile(models.Model):
+    name = models.CharField(max_length=32)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    
