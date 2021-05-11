4983502afb3298acfe381c84429a2d16788981b4
kelj0 <kelj0@protonmail.com>
2021-05-12 00:16:40 +0200

    add django login logout

--- savedots/app/models.py
+++ savedots/app/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-
-class User(models.Model):
+#Identifier is e-mail instead of user ID
+class User(AbstractBaseUser):
     username = models.CharField(max_length=64)
     email = models.EmailField()
     password = models.CharField()
-    
+
+    REQUIRED_FIELDS = ['username', 'email', 'password']
+
+    def __unicode__(self):
+        return self.username
+        
 class Dotfile(models.Model):
     name = models.CharField(max_length=32)
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
--- savedots/savedots/urls.py
+++ savedots/savedots/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from app import views
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
